2dcb497c54f22c32b832d7717a73e23d48fc271b
Roosivelt <roosivelt.td@gmail.com>
2025-11-06 23:15:10 -0500

    registro de Tipo de proceso V0.0.1

--- obj/Debug/net9.0/SistemaGestionAgricola.AssemblyInfo.cs
+++ obj/Debug/net9.0/SistemaGestionAgricola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGestionAgricola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eabee45d1340f4f23a7691bd758f7dae8f5da49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97790d68b0b00a41ac24f9f93948a0ac6fe120ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGestionAgricola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGestionAgricola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
